82093378b493d851fe16cb97fef2d6f35cf87c11
Philip Patton <ppatton35745@gmail.com>
2018-06-11 10:47:14 -0500

    challenge complete functions work

--- main.js
+++ main.js
@@ -1,15 +1,44 @@
 const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
-const planetEl = document.getElementById("planets");
-const planetUL = document.createElement("ul");
 
 const addPlanetNames = (first, index, array) => {
     const listItem = document.createElement("li");
-    listItem.textContent = first;
+    listItem.textContent = `The planet of ${first} is neat`;
     planetUL.appendChild(listItem);
 }
 
-planets.forEach(addPlanetNames);
+
+fancyPlanetArray = planets.map(function (planet){
+    return planet.charAt(0).toUpperCase() + planet.slice(1);
+});
+
+ePlanets = planets.filter(planet => {
+    return (planet.includes("e")) || (planet.search("E") >= 0);
+});
+
+const planetEl = document.getElementById("planets");
+const planetUL = document.createElement("ul");
 planetEl.appendChild(planetUL);
+
+planets.forEach(addPlanetNames);
+fancyPlanetArray.forEach(addPlanetNames);
+ePlanets.forEach(addPlanetNames);
+
+const words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"]
+
+const mySentence = words.reduce(
+    (currentSentence, next) => `${currentSentence} ${next}`
+)
+
+
+const myParagraph = document.createElement("p");
+myParagraph.textContent = mySentence;
+
+planetEl.appendChild(myParagraph);
+
+
+
+
+
 /*
     Use the forEach method to add the name of each planet
     to a section element in your HTML with an id of "planets".
@@ -38,4 +67,22 @@ planetEl.appendChild(planetUL);
 
 
 // Use the reduce method to create a sentence from the words in the following array
-const words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"]
+// const words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"]
+
+// const mySentence = words.reduce(
+//     (currentSentence, next) => `${currentSentence} ${next}`
+// )
+
+// const monthlyRainfall = [23, 32, 27, 20, 20, 31, 33, 26, 19, 12]
+
+// // ES6+ syntax
+// const totalRainfall = monthlyRainfall.reduce(
+//     (currentTotal, next) => currentTotal + next
+// )
+
+// // Traditional syntax
+// const totalRainfall = monthlyRainfall.reduce(
+//     function (currentTotal, next) {
+//         return currentTotal + next
+//     }
+// )
