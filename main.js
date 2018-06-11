45ab212537127944f0148d3662d5f852327dd928
Philip Patton <ppatton35745@gmail.com>
2018-06-11 09:58:18 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+const planetEl = document.getElementById("planets");
+const planetUL = document.createElement("ul");
+
+const addPlanetNames = (first, index, array) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = first;
+    planetUL.appendChild(listItem);
+}
+
+planets.forEach(addPlanetNames);
+planetEl.appendChild(planetUL);
+/*
+    Use the forEach method to add the name of each planet
+    to a section element in your HTML with an id of "planets".
+    Use string templates to construct the DOM elements.
+*/
+
+
+
+
+/*
+    Use the map method to create a new array where the
+    first letter of each planet is capitalized. Use the
+    `toUpperCase()` method on strings.
+
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+*/
+
+
+/*
+    Use the filter method to create a new array that
+    contains planets with the letter 'e'. Use the `includes()`
+    method on strings.
+
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
+*/
+
+
+// Use the reduce method to create a sentence from the words in the following array
+const words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"]
